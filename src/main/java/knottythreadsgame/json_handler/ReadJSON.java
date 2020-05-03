f1d4365e6bc9f08ca31153a662fb8e0413ea9efe
AnastasiaKremenetskaya <46082850+AnastasiaKremenetskaya@users.noreply.github.com>
2020-05-03 22:21:25 +0400

    Исправлен класс для обработки JSON

--- src/main/java/knottythreadsgame/json_handler/ReadJSON.java
+++ src/main/java/knottythreadsgame/json_handler/ReadJSON.java
@@ -1,11 +1,12 @@
 package knottythreadsgame.json_handler;
+
 import java.awt.geom.Point2D;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import knottythreadsgame.model.Knot;
 import org.json.simple.JSONArray;
@@ -16,30 +17,25 @@ import org.json.simple.parser.ParseException;
 public class ReadJSON {
 
     /**
-     *
      * @param difficultyLevel выбранный уровень сложности
      * @return
      */
     @SuppressWarnings("unchecked")
-    public List<Knot> getPositionsFromJson(String difficultyLevel)
-    {
+    public List<Knot> getPositionsFromJson(String difficultyLevel) {
         List<Knot> knots = new ArrayList<>();
 
-        JSONArray requiredLevelKnotsList;
+        JSONArray requiredLevelKnotsList = new JSONArray();
 
         //JSON parser object to parse read file
         JSONParser jsonParser = new JSONParser();
 
-        try (FileReader reader = new FileReader("src/main/resources/knots_positions.json"))
-        {
-            //Read JSON file
-            JSONObject jsonObject = (JSONObject) jsonParser.parse(reader);
+        try (FileReader reader = new FileReader("src/main/resources/knots_positions.json")) {
+            requiredLevelKnotsList.add(jsonParser.parse(reader));
 
-            //Считать из него позиции для нужного уровня
-            requiredLevelKnotsList = (JSONArray) jsonObject.get(difficultyLevel);
+            List<Object> valuesForGivenKey = getValuesForGivenKey(requiredLevelKnotsList, difficultyLevel);
 
             //Распарсить позицию каждого узла в контейнер узлов
-            parsePositionToArrayList(requiredLevelKnotsList, knots);
+            parsePositionToArrayList((List<Object>) valuesForGivenKey.get(0), knots);
 
         } catch (FileNotFoundException e) {
             e.printStackTrace();
@@ -53,15 +49,31 @@ public class ReadJSON {
     }
 
     /**
+     * Получить значения по заданному ключу
      *
-     * @param requiredLevelKnotsList уровень, для которого необходимо считать данные
-     * @param knots контейнер узлов
+     * @param jsonArray
+     * @param key
+     * @return
      */
-    private static void parsePositionToArrayList(JSONArray requiredLevelKnotsList, List<Knot>knots)
-    {
-        Iterator<Point2D> iterator = requiredLevelKnotsList.iterator();
-        while (iterator.hasNext()) {
-            knots.add(new Knot( iterator.next() ));
+    private List<Object> getValuesForGivenKey(JSONArray jsonArray, String key) {
+        return IntStream.range(0, jsonArray.size())
+                .mapToObj(index -> ((JSONObject) jsonArray.get(index)).get(key))
+                .collect(Collectors.toList());
+    }
+
+    /**
+     * @param valuesForGivenKey данные для уровня
+     * @param knots             контейнер узлов
+     */
+    private static void parsePositionToArrayList(List<Object> valuesForGivenKey, List<Knot> knots) {
+        Double x;
+        Double y;
+        Point2D.Double position;
+        for (int i = 0; i < valuesForGivenKey.size() - 1; i = i+2) {
+            x = (Double) valuesForGivenKey.get(i);
+            y = (Double) valuesForGivenKey.get(i+1);
+            position = new Point2D.Double(x, y);
+            knots.add(new Knot(position));
         }
     }
 }
